757bc68bc5a58d89d623c162b4289c14bf7429fd
luigimuzy <luigifracalanza@gmail.com>
2018-06-14 18:31:53 -0300

    Tentativas mal sucedidas de fazer index.php em apimagento/includefuncionar...

--- arquivo_para_testar_funcoes.php
+++ arquivo_para_testar_funcoes.php
@@ -1,11 +1,74 @@
 <?php
 include "include/all_include.php";
 
-$SKU = "EP-51-40983";
 
-$MLB = "1038933651";
+do {
+  $média_de_cada_passe = 0;
+  $tempo = time();
 
-var_dump(atualizaProdMLB($SKU,$MLB));
+  echo "lalalalala";
+  echo "Tempo :". (time() - $tempo);
+  echo "<h2>3 - Descobrir o SKU através do MLB do próximo</h2>";
+
+ sleep(2);// 3 - Descobrir o SKU através do MLB do próximo
+   echo "<br>";
+   echo "TEMPO: ". (time() - $tempo);
+   echo "<br><br><br></h2>";
+ $tempo_até_agora = (time() - $tempo);
+ $média_de_cada_passe =( $média_de_cada_passe+$tempo_até_agora)/2;
+echo $média_de_cada_passe;
+}
+while (($tempo_até_agora + 10) < 30);
+
+// do
+// {
+//   $time_inicial = time();
+//   echo "<pre><br>";
+//   echo "TEMPO: ". (time() - $time_inicial);
+//   echo "<br>";
+//
+//   echo "<h2>1 - Ler a lista de produtos no ML (arquivo list.php)  -- IMPLICITO NO PROXIMO PASSO</h2>";
+//   echo "<br>";
+//
+//   echo "<h2>2 - Localizar ultimo (json) e o próximo da lista (array que vem do list)</h2>";
+//   echo "<br>";
+//   $MLB = proximo_MLB(); // 2 - Localizar ultimo (json) e o próximo da lista (array que vem do list)
+//   $next_MLB = ultimo_MLB();
+//   echo "<h2>ATUAL MLB: $MLB<BR>ANTERIOR MLB: $next_MLB  <BR>";
+//   echo "TEMPO:". (time() - $time_inicial);
+//   echo "<br><br><br></h2>";
+//
+//   echo "<h2>3 - Descobrir o SKU através do MLB do próximo</h2>";
+//   $SKU = retorna_SKU($MLB); // 3 - Descobrir o SKU através do MLB do próximo
+//   echo "<br>";
+//   echo "<h2>ATUAL SKU: ". $SKU ."<BR>PROXIMO SKU:". retorna_SKU($next_MLB) ."<BR>";
+//   echo "TEMPO: ". (time() - $time_inicial);
+//   echo "<br><br><br></h2>";
+//
+//   if((isset($MLB) && isset($SKU)) || ($MLB != 0 && $SKU != 0))
+//   {
+//     echo "<h2>4 - Rodar a função de atualização com os dois dados</h2>";
+//     echo "<br>";
+//     $atualiza = atualizaMLB($SKU,$MLB); // 4 - Rodar a função de atualização com os dois dados
+//     echo "<h2>atualiza: $atualiza <BR>";
+//   }
+//   echo "TEMPO: ". (time() - $time_inicial);
+//   echo "<br><br><br></h2>";
+//   // if($atualiza)
+//   // {
+//     echo "<h2>5 - Escrever o ultimo MLB atualizado no json</h2>";
+//     echo "<br>";
+//     $escreveMLB = escreve_MLB($MLB); // 5 - Escrever o ultimo no json
+//     echo "<h2>escreveMLB: $escreveMLB <BR>";
+//   // }
+//
+//   echo "TEMPO: ". (time() - $time_inicial);
+//   $tempo_até_agora = (time() - $time_inicial);
+//   $média_de_cada_passe = $média_de_cada_passe+$tempo_até_agora;
+//
+//   echo "<br><br><br></h2>";
+// }
+// while (($tempo_até_agora + $média_de_cada_passe) < 60);
 
 // USUARIO DE TESTES
 //  ["id"]=> int(327485416)
