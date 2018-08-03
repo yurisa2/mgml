562d20ae56a896d9e617c2e3b9ff95e51b33b413
yurisa2 <yuri@sa2.com.br>
2018-08-03 10:03:01 -0300

    f5 impro

--- git_f5.php
+++ git_f5.php
@@ -3,6 +3,9 @@
 $git_reset = shell_exec("git reset --hard");
 var_dump($git_reset);
 
+$git_stash = shell_exec("git stash");
+var_dump($git_stash);
+
 $git_pull = shell_exec("git pull");
 var_dump($git_pull);
 
