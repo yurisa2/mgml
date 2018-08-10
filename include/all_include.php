c57aa76044c0b15eeadf6a01db1880b9ebbe3c1a
luigi <luigifracalanza@gmail.com>
2018-08-10 17:58:58 -0300

    commit teste

--- include/all_include.php
+++ include/all_include.php
@@ -1,35 +1,26 @@
 <?php
-echo "1";
 require_once 'include/config.php';
-echo "2";
 require_once 'include/funcoes.php';
-echo "3";
 require_once 'include/orderAdd.php';
-echo "4";
 require_once "include/event_base.php";
-echo "5";
 require_once 'include/error_handling.php';
-echo "6";
 require_once 'include/log.php';
-echo "7";
 
 require_once 'include/ml/php-sdk/Meli/meli.php';
-echo "8";
 require_once 'include/ml/php-sdk/configApp.php';
-echo "9";
 require_once 'include/apimagentophp/include/all_include.php';
-echo "10";
+
 
 require_once 'include/PHPMailer/src/Exception.php';
-echo "11";
+
 require_once 'include/PHPMailer/src/PHPMailer.php';
-echo "12";
+
 require_once 'include/PHPMailer/src/SMTP.php';
-echo "13";
+
 
 require_once 'include/mail/mail.php';
-echo "14";
+
 require_once 'include/mail/corpo_email.php';
-echo "15";
+
 
  ?>
