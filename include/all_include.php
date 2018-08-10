b4d1475817877ec77ec26e077673d347353f992e
luigi <luigifracalanza@gmail.com>
2018-08-10 17:51:18 -0300

    echo teste

--- include/all_include.php
+++ include/all_include.php
@@ -1,21 +1,35 @@
 <?php
-require 'include/config.php';
-require 'include/funcoes.php';
-require 'include/orderAdd.php';
+echo "1";
+require_once 'include/config.php';
+echo "2";
+require_once 'include/funcoes.php';
+echo "3";
+require_once 'include/orderAdd.php';
+echo "4";
+require_once "include/event_base.php";
+echo "5";
+require_once 'include/error_handling.php';
+echo "6";
+require_once 'include/log.php';
+echo "7";
 
-require "include/event_base.php";
-require 'include/error_handling.php';
-require 'include/log.php';
+require_once 'include/ml/php-sdk/Meli/meli.php';
+echo "8";
+require_once 'include/ml/php-sdk/configApp.php';
+echo "9";
+require_once 'include/apimagentophp/include/all_include.php';
+echo "10";
 
-require 'include/ml/php-sdk/Meli/meli.php';
-require 'include/ml/php-sdk/configApp.php';
-require 'include/apimagentophp/include/all_include.php';
+require_once 'include/PHPMailer/src/Exception.php';
+echo "11";
+require_once 'include/PHPMailer/src/PHPMailer.php';
+echo "12";
+require_once 'include/PHPMailer/src/SMTP.php';
+echo "13";
 
-require 'include/PHPMailer/src/Exception.php';
-require 'include/PHPMailer/src/PHPMailer.php';
-require 'include/PHPMailer/src/SMTP.php';
-
-require 'include/mail/mail.php';
-require 'include/mail/corpo_email.php';
+require_once 'include/mail/mail.php';
+echo "14";
+require_once 'include/mail/corpo_email.php';
+echo "15";
 
  ?>
