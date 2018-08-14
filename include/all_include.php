1a3aae5eee70e00781745386819dd1771e38455a
luigi <luigifracalanza@gmail.com>
2018-08-14 18:24:27 -0300

    reformulação para ver melhor

--- include/all_include.php
+++ include/all_include.php
@@ -2,7 +2,7 @@
 require_once 'include/config.php';
 require_once 'include/funcoes.php';
 require_once 'include/orderAdd.php';
-require_once "include/event_base.php";
+require_once 'include/event_base.php';
 require_once 'include/error_handling.php';
 require_once 'include/log.php';
 
@@ -10,17 +10,11 @@ require_once 'include/ml/php-sdk/Meli/meli.php';
 require_once 'include/ml/php-sdk/configApp.php';
 require_once 'include/apimagentophp/include/all_include.php';
 
-
 require_once 'include/PHPMailer/src/Exception.php';
-
 require_once 'include/PHPMailer/src/PHPMailer.php';
-
 require_once 'include/PHPMailer/src/SMTP.php';
 
-
 require_once 'include/mail/mail.php';
-
 require_once 'include/mail/corpo_email.php';
-
 
  ?>
