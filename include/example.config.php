d31f5347dbb6e348f4875f0b05eeba9d1a1be37c
luigi <luigifracalanza@gmail.com>
2018-09-27 18:14:52 -0300

    template do config.php

--- include/example.config.php
+++ include/example.config.php
@@ -1,25 +1,24 @@
 <?php
 //MAGENTO
-$prefix_m = 'include/apimagentophp/';
+$prefix_m = '';
 
 //ML
 $app_Id = "";
 $secret_Key = "";
 $user_id = "";
-$site_id = "MLB";
+$site_id = "";
 
 //Ajustes
-$ajuste_preco_multiplicacao = "1.3";
-$ajuste_estoque = "-0.2";
-$sufixo_prod = " - EasyPath";
+$ajuste_preco_multiplicacao = "";
+$ajuste_estoque = "";
+$sufixo_prod = "";
 $prefixo_prod = "";
-$marca = "EasyPath";
-$ajuste_preco_soma = "5.0";
+$marca = "";
+$ajuste_preco_soma = "";
 
 //ML
 $configmail = true;     // true para habilitar o envio de email
-$email_destinatario = array('1'=>"",
-                            '2'=>"");
+$email_destinatario;
 
 // Ainda há problemas não encontrados para o uso do Sendmail
 $SMTP = false;              //se SMTP é false, sendmail será usado
